3d97e0de1d725e8ed24b2758ea7094eaa37b644f
“reddrum” <anton.kranz@me.com>
2017-09-02 17:38:56 +0300

    Add model article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    @article = Article.new(params.require(:article).permit(:title, :text))
   end
 
 end
